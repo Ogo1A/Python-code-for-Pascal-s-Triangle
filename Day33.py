14e0bf9f69b91da44ba65926f28a3c000cd9ba0e
Darren Ogola <darren.sunday@strathmore.edu>
2024-11-27 23:04:22 +0300

    My Day33 of 365 challange

---
+++ Day33.py
@@ -0,0 +1,15 @@
+def printPascal(N):
+arr = [1)
+temp = []
+print ("pascal's triangle of", N, "Rows...") for 1 in range(N):
+print("rows", i+1, end=": *)
+    for j in range(len(arr)):
+print(arr[jl, end=' ')
+print()
+#clcoding.com
+temp.append (1)
+temp.append (1)
+arr = temp
+temp = []
+N=int(input("Enter the Number for the pascal triangle :"))
+printPascal(N)
